c2b8494398dff41c7913260103d57c57bb22a11e
Nuwan Subhasingha <126569796+NuwanSubasingha99@users.noreply.github.com>
2024-02-24 19:11:00 +0530

    display-login-email

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
@@ -8,13 +9,22 @@ import { ToastrService } from 'ngx-toastr';
 export class AuthService {
 
   constructor(private afAuth: AngularFireAuth,
-    private toster: ToastrService) { }
+    private toster: ToastrService,
+    private router: Router) { }
 
   login(email:string,password:string){
     this.afAuth.signInWithEmailAndPassword(email,password).then(logRef=>{
-        this.toster.success("Login successfull..")
+        this.toster.success("Login successfull..");
+        this.loadUser();
+        this.router.navigate(['/'])
     }).catch(e=>{
       this.toster.warning(e);
+    })
+  }
+
+  loadUser(){
+    this.afAuth.authState.subscribe(user =>{
+      localStorage.setItem('user',JSON.stringify(user));
     })
   }
 }
