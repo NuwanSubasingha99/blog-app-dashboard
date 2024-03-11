20635bed358f4745b35247baf10799f19e1ae6ce
Nuwan Subhasingha <126569796+NuwanSubasingha99@users.noreply.github.com>
2024-03-11 22:58:26 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: "AIzaSyCyDBq0f__q6nPGwKnJsEWlbNmR_qpMWZU",
-        authDomain: "blog-app99.firebaseapp.com",
-        projectId: "blog-app99",
-        storageBucket: "blog-app99.appspot.com",
-        messagingSenderId: "164178595393",
-        appId: "1:164178595393:web:0ea11ff1b6fb8e7614c0c0"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
       }
 };
